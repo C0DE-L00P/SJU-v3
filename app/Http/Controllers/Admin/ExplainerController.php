2d37d7cc4a3813637aadf291171aaf2f7cfb4700
mohamed ebada <p.eng.mohamedebada@gmail.com>
2024-01-24 22:47:44 +0200

    edits

---
+++ app/Http/Controllers/Admin/ExplainerController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Explainer;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\ExplainerRequest;
+use App\Http\Resources\ExplainerResource;
+use App\Models\TrainingBag;
+
+class ExplainerController extends Controller
+{
+    public function __construct()
+    {
+        $this->authorizeResource(TrainingBag::class, 'file');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(): \Inertia\Response|\Inertia\ResponseFactory
+    {
+        $files = Explainer::filter(request())
+            ->order(request())
+            ->paginate(request()->perPage ?: 10)
+            ->withQueryString();
+
+        return inertia('Admin/Explainer/Index', [
+            'files' => ExplainerResource::collection($files)->additional([
+                'can_create' => request()->user()->can('create', TrainingBag::class),
+            ]),
+            'filters' => request()->only(['perPage', 'search', 'order', 'dir'])
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return inertia('Admin/Explainer/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ExplainerRequest $request)
+    {
+        $data = $request->validated();
+
+        // Store it
+        $data['path'] = $data['file']->store("explainer");
+
+        // Create file
+        $file = Explainer::create($data);
+
+        // Redirect
+        return redirect()->route('admin.explainer.index')->with('message', __('You submitted successfully'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Explainer $file)
+    {
+        // Delete files
+        Storage::delete($file->path ?? '');
+        $file->delete();
+        return redirect()->back()->with('message', __('File deleted successfully'));
+    }
+}
---
+++ resources/js/Pages/Pages/Explainer.vue
@@ -0,0 +1,60 @@
+<script setup>
+import Pagination from '../Components/Pagination.vue';
+const props = defineProps({
+    explainers: Object,
+    locale: String,
+});
+</script>
+
+<template>
+    <Head :title="__('Explainers')" />
+    <div class="sjupage allnews">
+        <div class="section page-header">
+            <div class="container">
+                <nav aria-label="breadcrumb">
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <Link :href="route('home')">{{ __('Home') }}</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <h3>{{ __('Explainers') }}</h3>
+            </div>
+        </div>
+
+        <div class="page-content">
+            <div class="container">
+                <template v-if="explainers.data.length">
+                    <div class="tab-content" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                            <div class="row">
+                                <div class="col-sm-6 col-md-4 mb-4" v-for="explainer in explainers.data" :key="explainer.id">
+
+                                    <div class="card text-center h-100">
+                                        <div class="card-body">
+                                            <Link :href="explainer.path" as="h5" style="height: 50px" class="card-title">{{
+                                                explainer.name }}</Link>
+                                            <a :href="explainer.path" class="btn btn-success btn-sm mt-2 ripple-surface" target="_blank">
+                                                {{ __('View') }}
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </template>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script defer>
+import App from '../../Layouts/App.vue';
+export default {
+    layout: App,
+    mounted() { },
+};
+</script>
